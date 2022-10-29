f478ec7d855c8659a125e0ae92ceb56ecc782e74
jakubfiglak <jakub.figlak@netguru.pl>
2022-10-29 22:11:11 +0200

    feat: add basic color palette

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -8,6 +8,78 @@ module.exports = {
       fontFamily: {
         sans: ['League Spartan', ...defaultTheme.fontFamily.sans],
       },
+      fontSize: {
+        h1: [
+          '2rem',
+          {
+            lineHeight: '2.25rem',
+            letterSpacing: '-0.03125em',
+            fontWeight: '700',
+          },
+        ],
+        h2: [
+          '1.25rem',
+          {
+            lineHeight: '1.375rem',
+            letterSpacing: '-0.0315em',
+            fontWeight: '700',
+          },
+        ],
+        h3: [
+          '1rem',
+          {
+            lineHeight: '1.5rem',
+            letterSpacing: '-0.05em',
+            fontWeight: '700',
+          },
+        ],
+        h4: [
+          '0.75rem',
+          {
+            lineHeight: '0.9375rem',
+            letterSpacing: '-0.02em',
+            fontWeight: '700',
+          },
+        ],
+        body1: [
+          '0.75rem',
+          {
+            lineHeight: '0.9375rem',
+            letterSpacing: '-0.02em',
+            fontWeight: '500',
+          },
+        ],
+        body2: [
+          '0.6875rem',
+          {
+            lineHeight: '1.125rem',
+            letterSpacing: '-0.02em',
+            fontWeight: '500',
+          },
+        ],
+      },
+    },
+    colors: {
+      black: '#0C0E16',
+      purple: {
+        dark: '#7C5DFA',
+        DEFAULT: '#9277FF',
+        light: '#7E88C3',
+        extraLight: '#DFE3FA',
+      },
+      gray: {
+        extraDark: '#141625',
+        dark: '#1E2139',
+        DEFAULT: '#252945',
+        light: '#888EB0',
+        extraLight: '#F8F8F8',
+      },
+      red: {
+        DEFAULT: '#EC5757',
+        light: '#FF9797',
+      },
+      green: '#33D69F',
+      orange: '#FF8F00',
     },
   },
   plugins: [],
--- web/src/components/Test/Test.tsx
+++ web/src/components/Test/Test.tsx
@@ -1,7 +1,7 @@
 const Test = () => {
   return (
     <div>
-      <h2 className="text-9xl font-bold">Spartan</h2>
+      <h2 className="text-h1">Spartan</h2>
       <p>{'Find me in ./web/src/components/Test/Test.tsx'}</p>
     </div>
   )
