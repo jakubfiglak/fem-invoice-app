6b84e011aba471b5f91104495c2fcecf090379c9
jakubfiglak <jakub.figlak@netguru.pl>
2022-10-29 21:35:42 +0200

    feat: add spartan font

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -12,6 +12,7 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/test" page={TestPage} name="test" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
---
+++ web/src/components/Test/Test.tsx
@@ -0,0 +1,10 @@
+const Test = () => {
+  return (
+    <div>
+      <h2 className="text-9xl font-bold">Spartan</h2>
+      <p>{'Find me in ./web/src/components/Test/Test.tsx'}</p>
+    </div>
+  )
+}
+
+export default Test
